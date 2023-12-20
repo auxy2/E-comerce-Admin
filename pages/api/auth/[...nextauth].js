983153a3e836214f7b06179d7fe95a27c8f1bd65
ABDULLAHI <Ashleyalvins010@gmail.com>
2023-12-20 20:16:24 +0100

    Updated

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,42 @@
+import NextAuth, { getServerSession } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
+
+const adminEmails = ["dawid.paszko@gmail.com"];
+
+export const authOptions = {
+  secret: process.env.SECRET,
+  providers: [
+    GoogleProvider({
+      clientId:
+        "613376763309-aaejp6psih593ojik5uiqth5mok4qf5b.apps.googleusercontent.com",
+      // process.env.GOOGLE_ID,
+      clientSecret: "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",
+      // api Key: "AIzaSyC7u52oqCUeQLBd7m6WrWF9wtBBsISJLkE",
+      // process.env.GOOGLE_SECRET,
+    }),
+  ],
+  // mfhjloaeznxnohqa
+  adapter: MongoDBAdapter(clientPromise),
+  callbacks: {
+    session: ({ session, token, user }) => {
+      if (adminEmails.includes(session?.user?.email)) {
+        return session;
+      } else {
+        return false;
+      }
+    },
+  },
+};
+
+export default NextAuth(authOptions);
+
+export async function isAdminRequest(req, res) {
+  const session = await getServerSession(req, res, authOptions);
+  if (!adminEmails.includes(session?.user?.email)) {
+    res.status(401);
+    res.end();
+    throw "not an admin";
+  }
+}
